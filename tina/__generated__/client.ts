54593438e09fae8ec43b380c12274a6efd6a8a73
Daniel Martinez <dmartinez4@atlassian.com>
2026-06-05 16:48:37 -0600

    Replace Tina with Decap CMS

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '7e8da812de656dd5880684852be58d99dc7a539e', queries,  });
-export default client;
-  
