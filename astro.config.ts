0a5d75ee5c25b1e52ce5e752259daa8e7e62508e
Daniel Martinez <sarekmartinez53@gmail.com>
2026-06-05 21:24:30 -0600

    Clean up CMS, fix SEO, fix Giscus config, make image optional

--- astro.config.ts
+++ astro.config.ts
@@ -2,23 +2,9 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
-import { loadEnv } from "vite";
 import spectre, { type GiscusMapping } from "./package/src";
 import { spectreDark } from "./src/ec-theme";
 
-const {
-	GISCUS_REPO,
-	GISCUS_REPO_ID,
-	GISCUS_CATEGORY,
-	GISCUS_CATEGORY_ID,
-	GISCUS_MAPPING,
-	GISCUS_STRICT,
-	GISCUS_REACTIONS_ENABLED,
-	GISCUS_EMIT_METADATA,
-	GISCUS_LANG,
-} = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
-
-// https://astro.build/config
 const config = defineConfig({
 	site: "https://eldanmtz.com",
 	output: "static",
@@ -33,14 +19,14 @@ const config = defineConfig({
 			openGraph: {
 				home: {
 					title: "Dan Mtz",
-					description: "Write something funny",
+					description: "Blog personal — Tech, Cocina, Gaming y la vida.",
 				},
 				blog: {
 					title: "Blog",
-					description: "Bloggin on 2026? what are you? Old?",
+					description: "Blog personal — Tech, Cocina, Gaming y la vida.",
 				},
 				projects: {
-					title: "Projects",
+					title: "Projectos",
 				},
 			},
 			giscus: {
@@ -49,9 +35,9 @@ const config = defineConfig({
 				category: "Announcements",
 				categoryId: "DIC_kwDOSxpI3s4C-iya",
 				mapping: "pathname" as GiscusMapping,
-				strict: "1" === "true",
-				reactionsEnabled: "1" === "true",
-				emitMetadata: "0" === "true",
+				strict: false,
+				reactionsEnabled: true,
+				emitMetadata: false,
 				lang: "en",
 			},
 		}),
